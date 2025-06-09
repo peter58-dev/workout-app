b9668a6ba7040febed58e4feea52853299959fd1
peter-lelkes <p.lelkes@gmail.com>
2025-06-09 21:30:00 +0200

    firebaseconfig inklistrad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCYCOMRjP5uw05meHp0S3TDPQjaSl2LPKs',
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+    authDomain: 'strength-app-cd974.firebaseapp.com',
+
+    projectId: 'strength-app-cd974',
+
+    storageBucket: 'strength-app-cd974.firebasestorage.app',
+
+    messagingSenderId: '588939477744',
+
+    appId: '1:588939477744:web:16a9334867c35fde51f835',
+  },
+};
